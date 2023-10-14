6c511c75ed3dbbfe42d2fa06983235867a222de6
Jordan <jordandysart@live.ca>
2023-10-14 02:56:19 -0500

    update copy used in layout

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
 					enjoy the experience.
 				</h1>
 				<h2 className={subtitle({ class: "mt-4" })}>
-					Study, Teach, and Practicing Tools implemented with Modern Web.
+					Study, Teach, and Practice using tools from the Modern Web.
 				</h2>
 			</div>
 
@@ -43,15 +43,6 @@ export default function Home() {
 					GitHub
 				</Link>
 			</div>
-
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
-			</div>
-
 
 			
 			<Counter />
