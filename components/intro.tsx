06bf9fd90af8db93e0ad5d4745b03379fefe007e
Jordan <jordandysart@live.ca>
2023-10-15 22:43:27 -0500

    fix type errors for new objects

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,23 +5,35 @@ import { SymbolTable } from "./symbol-table";
 import { unifiedCanadianAboriginalSyllabicsObject as symbols, languages} from "../_data/unicode";
 import { useEffect, useState } from "react";
 
+type Symbol = {
+		symbol: string,
+		title: string,
+		description: string,
+		language: string,
+		unicodeValue: string,
+}
+
+type SymbolCollection = typeof symbols;
+type LangIndex = keyof SymbolCollection;
+type SymbolType = {[key:string ]: Symbol}
+
 export const Intro = () => {
 
-	const [language, setLanguage] = useState("CANADIAN SYLLABICS");
-	const [symbolsSet, setSymbolsSet] = useState( symbols[language]);
+	const [language, setLanguage] = useState<LangIndex>("CANADIAN SYLLABICS");
+	const [symbolsSet, setSymbolsSet] = useState<SymbolType>(symbols[language]);
 	const intro = "Check it out, these groupos of syllabics are referenced in the unicode standard. The generic group of CANADIAN SYLLABICS seem to be used throughout canada while some dialects and langauges have specific symbols that they rely on. Neat!";
 
 	const handleChange = (e) => {
 		setLanguage(event.target.value)
 	}
 
-	useEffect( () => {
-		setSymbolsSet(symbols[language])
-		},[language, setSymbolsSet])
+	useEffect(() => {
+		setSymbolsSet(symbols[language] as SymbolType)
+	}, [language, setSymbolsSet])
 
 	return (
 		<>
-			<div className="flex flex-row gap-3 my-unit-5 py-2">	
+			<div className="flex flex-row gap-3 my-unit-5 py-2">
 				<div className="flex flex-col gap-3 w-unit-9xl">
 					<div className="flex gap-3">
 						{`${intro} `}
@@ -30,10 +42,10 @@ export const Intro = () => {
 			</div>
 			<select value={language} onChange={handleChange}>
 				{
-					languages.map( (item: string) => <option key={item} value={item} >{item}</option>)
+					languages.map((item: string) => <option key={item} value={item} >{item}</option>)
 				}
 			</select>
-				
+
 
 			<SymbolTable symbols={symbolsSet} />
 		</>
